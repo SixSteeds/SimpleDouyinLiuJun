9666b6ba45dc2a3e20db83ff9f6af476bbc56390
zhanghao <2573344091@qq.com>
2023-08-23 15:30:23 +0800

    完成视频抽帧提取封面功能

--- service/media/api/internal/logic/uploadLogic.go
+++ service/media/api/internal/logic/uploadLogic.go
@@ -10,6 +10,7 @@ import (
 	util2 "doushen_by_liujun/service/media/rpc/util"
 	"fmt"
 	"github.com/google/uuid"
+	"log"
 	"strconv"
 
 	"github.com/zeromicro/go-zero/core/logx"
@@ -40,7 +41,7 @@ func (l *UploadLogic) Upload(req *types.UploadReq) (resp *types.UploadResp, err 
 		}, nil
 	}
 	//生成文件名
-	fileName := "doushen/" + strconv.FormatInt(token.UserID, 10) + uuid.New().String()[:5] + ".mp4"
+	fileName := strconv.FormatInt(token.UserID, 10) + uuid.New().String()[:5]
 	err = util2.Upload(l.ctx, req.Data, fileName)
 	fmt.Println("上传成功")
 	if err != nil {
@@ -49,8 +50,24 @@ func (l *UploadLogic) Upload(req *types.UploadReq) (resp *types.UploadResp, err 
 			StatusCode: common.SERVER_COMMON_ERROR,
 		}, nil
 	}
+	// 抽取视频第 5 帧
+	//coverData, err := util2.GetFrame(fileName, 5)
+	util2.GetFrameByDocker(fileName)
+	if err != nil {
+		log.Printf("抽帧封面出问题:%v", err)
+		return &types.UploadResp{
+			StatusMsg:  common.MapErrMsg(common.SERVER_COMMON_ERROR),
+			StatusCode: common.SERVER_COMMON_ERROR,
+		}, nil
+	}
+	// 上传封面
+	//util2.PutPicture(coverData, fileName)
+	fmt.Println("上传封面")
+	util2.PutPictureByDocker(fileName)
+	fmt.Println("上传封面")
 	data, err := util.NewSnowflake(common.MediaApiMachineId)
 	if err != nil {
+		log.Println("上传封面出问题")
 		return &types.UploadResp{
 			StatusMsg:  common.MapErrMsg(common.SERVER_COMMON_ERROR),
 			StatusCode: common.SERVER_COMMON_ERROR,
@@ -59,8 +76,8 @@ func (l *UploadLogic) Upload(req *types.UploadReq) (resp *types.UploadResp, err 
 	snowId := data.Generate()
 	_, err = l.svcCtx.MediaRpcClient.SaveVideo(l.ctx, &pb.SaveVideoReq{
 		UserId:   token.UserID,
-		PlayUrl:  common.QiliuyunDomain + fileName,
-		CoverUrl: common.DefaultBackImage,
+		PlayUrl:  common.HTTP + common.MinIOEndPoint + "/" + common.MinIOVideoBucketName + "/" + fileName + ".mp4",
+		CoverUrl: common.HTTP + common.MinIOEndPoint + "/" + common.MinIOCoverBucketName + "/" + fileName + ".jpg",
 		Title:    req.Title,
 		Id:       snowId,
 	})
