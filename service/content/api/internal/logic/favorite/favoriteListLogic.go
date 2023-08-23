5e5f179be4d2cd9cb6eb094e9e69fcfed5f6a1aa
DMJYY <1137324853@qq.com>
2023-08-23 08:57:27 +0800

    简化喜欢列表，只保留视频封面和被点赞数

--- service/content/api/internal/logic/favorite/favoriteListLogic.go
+++ service/content/api/internal/logic/favorite/favoriteListLogic.go
@@ -11,7 +11,6 @@ import (
 	"doushen_by_liujun/service/content/api/internal/svc"
 	"doushen_by_liujun/service/content/api/internal/types"
 	"doushen_by_liujun/service/content/rpc/pb"
-	userPB "doushen_by_liujun/service/user/rpc/pb"
 	"fmt"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/stores/redis"
@@ -36,7 +35,7 @@ func (l *FavoriteListLogic) FavoriteList(req *types.FavoriteListReq) (resp *type
 	/*
 		Author：    刘洋
 		Function：  在用户界面返回点赞列表：视频1 [作者（用户信息）、视频信息]、视频2[...]、...
-		Update：    08.21
+		Update：    08.23
 	*/
 	redisClient := l.svcCtx.RedisClient
 
@@ -87,65 +86,67 @@ func (l *FavoriteListLogic) FavoriteList(req *types.FavoriteListReq) (resp *type
 	fmt.Println("查到videoList")
 
 	// 4.依次根据 videoList 中的作者id 查询 user 表，得到所有视频作者的 userInfo
-	var authorList []*userPB.Userinfo
-	for _, video := range videoListResp.GetVideoList() {
-		userResp, err3 := l.svcCtx.UserRpcClient.GetUserById(l.ctx, &userPB.GetUserByIdReq{
-			UserID: video.UserId,
-		})
-		if err3 != nil && err3 != sql.ErrNoRows {
-			return &types.FavoriteListResp{
-				StatusCode: common.DB_ERROR,
-				StatusMsg:  common.MapErrMsg(common.DB_ERROR),
-			}, nil
-		}
-		authorList = append(authorList, userResp.GetUserinfo())
-	}
-	fmt.Println("查到authorList")
+	//var authorList []*userPB.Userinfo
+	//for _, video := range videoListResp.GetVideoList() {
+	//	userResp, err3 := l.svcCtx.UserRpcClient.GetUserById(l.ctx, &userPB.GetUserByIdReq{
+	//		UserID: video.UserId,
+	//	})
+	//	if err3 != nil && err3 != sql.ErrNoRows {
+	//		return &types.FavoriteListResp{
+	//			StatusCode: common.DB_ERROR,
+	//			StatusMsg:  common.MapErrMsg(common.DB_ERROR),
+	//		}, nil
+	//	}
+	//	authorList = append(authorList, userResp.GetUserinfo())
+	//}
+	//fmt.Println("查到authorList")
 
 	// 4.同时遍历 videoListResp 和 userListResp ，对每个 user 调用服务组合获取计数信息（点赞数、作品数、获赞数）并作为当前遍历到的 video 的成员，
 	//   对每个 video 调用服务组合获取计数信息（点赞数、评论数），组装出完整的 video 对象，最终得到 videoList
 	var videoList []types.Video
 	for index, item := range videoListResp.GetVideoList() {
-		// 4.1 将 user 的计数信息组装到 user 对象
-		userLikeCntKey := constants.CntCacheUserLikePrefix + strconv.FormatInt(item.Id, 10)
-		// 4.1.1 从 redis 中拿到用户点赞数
-		userlikeCntRecord, err4 := redisClient.GetCtx(l.ctx, userLikeCntKey)
-		if err4 != nil && err4 != redis.Nil {
-			return &types.FavoriteListResp{
-				StatusCode: common.REDIS_ERROR,
-				StatusMsg:  common.MapErrMsg(common.REDIS_ERROR),
-			}, nil
-		}
-		var userLikeCnt int64 = 0
-		if len(userlikeCntRecord) != 0 {
-			userLikeCnt, _ = strconv.ParseInt(userlikeCntRecord, 10, 64)
-		}
-		// 4.1.2 根据 userId 调用一个服务组合查询 mysql 多张表，得到用户的发布作品数、获赞数
-		cntResp, err5 := l.svcCtx.ContentRpcClient.GetUserPublishAndLikedCntById(l.ctx, &pb.GetUserPublishAndLikedCntByIdReq{
-			UserId: authorList[index].Id,
-		})
-		if err5 != nil && err5 != sql.ErrNoRows {
-			return &types.FavoriteListResp{
-				StatusCode: common.DB_ERROR,
-				StatusMsg:  common.MapErrMsg(common.DB_ERROR),
-			}, nil
-		}
-		fmt.Println("查到publishCnt、likedCnt")
-		// 4.1.3 组装出完整的 user 对象
-		user := types.User{
-			Id:   item.Id,
-			Name: authorList[index].Username,
-			//FollowCount:     userResp.Userinfo.FollowCount,//关注数
-			//FollowerCount:   userResp.Userinfo.FollowerCount,//被关注数
-			//IsFollow:        userResp.Userinfo.IsFollow,//当前用户是否关注当前视频作者
-			Avatar:          authorList[index].Avatar,
-			BackgroundImage: authorList[index].BackgroundImage,
-			Signature:       authorList[index].Signature,
-			WorkCount:       cntResp.PublishCnt, //作品数量
-			FavoriteCount:   userLikeCnt,        //点赞数量
-			TotalFavorited:  cntResp.LikedCnt,   //获赞数量
-		}
+		fmt.Println(index)
+		//// 4.1 将 user 的计数信息组装到 user 对象
+		//userLikeCntKey := constants.CntCacheUserLikePrefix + strconv.FormatInt(item.Id, 10)
+		//// 4.1.1 从 redis 中拿到用户点赞数
+		//userlikeCntRecord, err4 := redisClient.GetCtx(l.ctx, userLikeCntKey)
+		//if err4 != nil && err4 != redis.Nil {
+		//	return &types.FavoriteListResp{
+		//		StatusCode: common.REDIS_ERROR,
+		//		StatusMsg:  common.MapErrMsg(common.REDIS_ERROR),
+		//	}, nil
+		//}
+		//var userLikeCnt int64 = 0
+		//if len(userlikeCntRecord) != 0 {
+		//	userLikeCnt, _ = strconv.ParseInt(userlikeCntRecord, 10, 64)
+		//}
+		//// 4.1.2 根据 userId 调用一个服务组合查询 mysql 多张表，得到用户的发布作品数、获赞数
+		//cntResp, err5 := l.svcCtx.ContentRpcClient.GetUserPublishAndLikedCntById(l.ctx, &pb.GetUserPublishAndLikedCntByIdReq{
+		//	UserId: authorList[index].Id,
+		//})
+		//if err5 != nil && err5 != sql.ErrNoRows {
+		//	return &types.FavoriteListResp{
+		//		StatusCode: common.DB_ERROR,
+		//		StatusMsg:  common.MapErrMsg(common.DB_ERROR),
+		//	}, nil
+		//}
+		//fmt.Println("查到publishCnt、likedCnt")
+		//// 4.1.3 组装出完整的 user 对象
+		//user := types.User{
+		//	Id:   item.Id,
+		//	Name: authorList[index].Username,
+		//	//FollowCount:     userResp.Userinfo.FollowCount,//关注数
+		//	//FollowerCount:   userResp.Userinfo.FollowerCount,//被关注数
+		//	//IsFollow:        userResp.Userinfo.IsFollow,//当前用户是否关注当前视频作者
+		//	Avatar:          authorList[index].Avatar,
+		//	BackgroundImage: authorList[index].BackgroundImage,
+		//	Signature:       authorList[index].Signature,
+		//	WorkCount:       cntResp.PublishCnt, //作品数量
+		//	FavoriteCount:   userLikeCnt,        //点赞数量
+		//	TotalFavorited:  cntResp.LikedCnt,   //获赞数量
+		//}
 		// 4.2 将 user 和视频计数信息添加到 video 对象，并加入 videoList
+
 		videoLikedCntKey := constants.CntCacheVideoLikedPrefix + strconv.FormatInt(item.Id, 10)
 		videoCommentedCntKey := constants.CntCacheVideoCommentedPrefix + strconv.FormatInt(item.Id, 10)
 		// 4.2.1 从 redis 获取当前 video 的点赞数
@@ -174,8 +175,8 @@ func (l *FavoriteListLogic) FavoriteList(req *types.FavoriteListReq) (resp *type
 		}
 		// 4.2.3 组装出完整的 video 对象，加入 videoList
 		videoList = append(videoList, types.Video{
-			Id:            item.Id,
-			Author:        user,
+			Id: item.Id,
+			//Author:        user,
 			PlayUrl:       item.PlayUrl,
 			CoverUrl:      item.CoverUrl,
 			FavoriteCount: videoLikedCnt,     //视频点赞数
